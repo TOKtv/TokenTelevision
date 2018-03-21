6a6a6d075dbd9b4d8b9b3fbd894faabddd815573
sullof <sullof@gmail.com>
2018-03-20 18:14:31 -0700

    Minor fixes

--- smartContract/test/helpers/assertRevert.js
+++ smartContract/test/helpers/assertRevert.js
@@ -2,10 +2,10 @@
 
 module.exports = async promise => {
   try {
-    await promise;
-    assert.fail('Expected revert not received');
+    await promise
+    assert.fail('Expected revert not received')
   } catch (error) {
-    const revertFound = error.message.search('revert') >= 0;
-    assert(revertFound, `Expected "revert", got ${error} instead`);
+    const revertFound = error.message.search('revert') >= 0
+    assert(revertFound, `Expected "revert", got ${error} instead`)
   }
-};
+}
--- smartContract/truffle.js
+++ smartContract/truffle.js
@@ -39,4 +39,4 @@ module.exports = {
       runs: 200
     }
   }
-};
+}
